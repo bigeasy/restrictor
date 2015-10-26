a025ce8e38bfd52c1edd0e824adab8e1fa80ae2a
Alan Gutierrez <alan@prettyrobots.com>
2015-10-26 01:08:16 -0400

    Copy implementation from Turnstile.
    Closes #6.

--- restrictor.js
+++ restrictor.js
@@ -1,1 +1,9 @@
-module.exports = 1
+var adhere = require('adhere'),
+    abend = require('abend')
+
+module.exports = function (method) {
+    return adhere(method, function (object, vargs) {
+        object._turnstile.enter(object, method, vargs, vargs.pop())
+        object._turnstile.nudge(abend)
+    })
+}
--- t/restrictor/restrictor.t.js
+++ t/restrictor/restrictor.t.js
@@ -1,5 +1,35 @@
 require('proof')(1, require('cadence')(prove))
 
 function prove (async, assert) {
-    assert(require('../..'), 'require')
+    var turnstile = require('turnstile')
+    var restrict = require('../..')
+    var abend = require('abend')
+
+    function Service () {
+        this._turnstile = new turnstile.Turnstile
+    }
+
+    Service.prototype.immediate = restrict(function (value, callback) {
+        callback(null, value)
+    })
+
+    Service.prototype.delayed = restrict(function (value, callback) {
+        setImmediate(callback, null, value)
+    })
+
+    Service.prototype.error = restrict(function (value, callback) {
+        throw new Error('thrown')
+    })
+
+    var service = new Service
+
+    async(function () {
+        service.delayed(1, async())
+        service.delayed(2, async())
+        service.immediate(3, async())
+    }, function (one, two, three) {
+        assert([ one, two, three ], [ 1, 2, 3 ], 'service')
+    }, function () {
+        service.error(function () {})
+    })
 }
